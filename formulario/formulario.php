c07152547a124813369e6711695e2ed33f915617
Tainara Santos <tainaravit.santos28@gmail.com>
2026-02-11 19:29:02 -0300

    Aula01 Intodução ao php

---
+++ formulario/formulario.php
@@ -0,0 +1,5 @@
+<?php 
+echo "<h1 style='color:red;'> Nome: {$_GET['nome']}<br></h1>"; /* pode usar chaves ou o .  */ 
+echo "<h1>CPF:" . $_GET['cpf'] . "</h1>"; /* Ponto é a concatenação {} */
+
+?>
